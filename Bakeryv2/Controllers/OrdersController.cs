c199f6ae2bd82de3208fe93616fd639ec9a8b8da
Erin Timlin <erintimlin@gmail.com>
2023-03-03 18:36:47 -0500

    update to account for orders

--- Bakeryv2/Controllers/OrdersController.cs
+++ Bakeryv2/Controllers/OrdersController.cs
@@ -7,23 +7,45 @@ namespace Bakeryv2.Controllers
   public class OrdersController : Controller
   {
 
-    [HttpGet("/vendors/{vendorId}/orders/new")]
-    public ActionResult New(int vendorId)
+    [HttpGet("/orders")]
+    public ActionResult Index()
+      {
+        List<Order> allOrders = Order.GetAll();
+        return View(allOrders);
+      }
+    
+    [HttpGet("/orders/new")]
+    public ActionResult New()
     {
-      Vendor vendor = Vendor.Find(vendorId);
-      return View(vendor);
+      return View();
     }
 
-
-    [HttpGet("/vendor/{vendorId}/order/{orderId}")]
-    public ActionResult Show(int vendorId, int orderId)
+    [HttpPost("/orders")]
+    public ActionResult Create(string orderName)
     {
-      Order order = Order.Find(orderId);
-      Vendor vendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderName);
+      {
+        return RedirectToAction("Index");
+      }
+    }
+
+    [HttpGet("/orders/{id}")]
+    public ActionResult Show(int id)
+    {
       Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("order", order);
-      model.Add("vendor", vendor);
+      Order selectedOrder = Order.Find(id);
+      List<Vendor> orderVendors = selectedOrder.Vendors;
+      model.Add("order", selectedOrder);
+      model.Add("vendor", selectedVendors);
       return View(model);
     }
   }
 }
+      // Order order = Order.Find(orderId);
+      // Vendor vendor = Vendor.Find(vendorId);
+    // [HttpGet("/vendors/{vendorId}/orders/new")]
+    // public ActionResult New(int vendorId)
+    // {
+    //   Vendor vendor = Vendor.Find(vendorId);
+    //   return View(vendor);
+    // }
