28f0c555989e99e889695da823481225f31328d0
Erin Timlin <erintimlin@gmail.com>
2023-03-03 18:21:56 -0500

    update vendors controller to account for orders

--- Bakeryv2/Controllers/VendorsController.cs
+++ Bakeryv2/Controllers/VendorsController.cs
@@ -4,58 +4,25 @@ using System.Collections.Generic;
 
 namespace Bakeryv2.Controllers
 {
+  //items=vendors category=orders
   public class VendorsController : Controller
   {
-    [HttpGet("/vendors")]
-    public ActionResult Index()
+    [HttpGet(".orders/{orderId/vendors/new}")]
+    public ActionResult New(int orderId)
     {
-      List<Vendor> allVendors = Vendor.GetAll();
-      return View(allVendors);
+      Order order = Order.Find(orderId);
+      return View(order);
     }
 
-    [HttpGet("/vendors/new")]
-    public ActionResult New()
+    [HttpGet("/orders/{orderId}/vendors/{vendorId}")]
+    public ActionResult Show(int orderId, int vendorId)
     {
-      return View();
-    }
-
-    [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName)
-    {
-      Vendor myVendor = new Vendor(vendorName);
-      return RedirectToAction("Index");
-    }
-
-  
-    [HttpGet("/vendors/{id}")]
-    public ActionResult Show(int id)
-    {
+      Vendor vendor = Vendor.Find(vendorId);
+      Order order = Order.Find(orderId);
       Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor selectedVendor = Vendor.Find(id);
-      List<Order> vendorOrders = selectedVendor.Orders;
-      model.Add("vendor", selectedVendor);
-      model.Add("order", vendorOrders);
+      model.Add("vendor", vendor);
+      model.Add("order", order);
       return View(model);
     }
-
-
-      //artist=vendor album=order
-    [HttpPost("/artists/{artistId}/albums")]
-    public ActionResult Create(int vendorId, string orderTitle)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderTitle);
-      foundVendor.AddOrder(newOrder);
-      List<Order> vendorOrder = foundVendor.Orders;
-      model.Add("order", vendorOrders);
-      model.Add("vendor", foundVendor);
-      return View("Show", model);
-    }
-
-  }
-}
-
-
   }
 }
